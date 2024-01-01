b1ba1991b77c39663647d1038303d5293f3939c7
JorgeVarea <vareaduranjorge@gmail.com>
2024-01-02 00:57:34 +0100

    dummy

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 13:35:12 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/02 00:49:02 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/02 00:57:09 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@
 #  define FD_LIMIT 1024
 # endif
 
-void	allocate_buffer_memory(char *buffer[FD_LIMIT], int fd, bool *ok);
-int		line_length(char *buffer_fd);
-char	*reallocate_line_memory(char *line, int new_line_len, bool *ok);
-char	*fill_line(char *buffer_fd, char *line, bool *full_line, bool *ok);
-void	delete_buffer_line(char *buffer[FD_LIMIT], int fd);
-char	*get_line(char *buffer_fd, bool *ok, bool *full_line);
-char	*get_next_line(int fd);
+void		allocate_buffer_memory(char *buffer[FD_LIMIT], int fd, bool *ok);
+int			line_length(char *buffer_fd);
+char		*reallocate_line_memory(char *line, int new_line_len, bool *ok);
+char		*fill_line(char *buffer_fd, char *line, bool *full_line, bool *ok);
+void		delete_buffer_line(char *buffer[FD_LIMIT], int fd);
+char		*get_line(char *buffer_fd, bool *ok, bool *full_line);
+char		*get_next_line(int fd);
 
 #endif
